3a26d8f6a5cba6bd0ca1d62a3bd00d424066ac06
Rihen <rihenww@gmail.com>
2026-06-05 09:29:30 +0530

    sitemap , robots.ts is added for ceo

---
+++ frontend/app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://thelostcabins.in',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://thelostcabins.in/rooms',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://thelostcabins.in/experiences',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://thelostcabins.in/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://thelostcabins.in/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
+}
