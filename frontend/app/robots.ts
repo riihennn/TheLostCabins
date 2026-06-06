6bd58f3e51d8a02f40b7440b596be226ff1729b6
Rihen <rihenww@gmail.com>
2026-06-06 09:28:15 +0530

    404 on google search console cache error         disallow: ['/_next/static/'], this is added

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/_next/static/'],
+      },
+    ],
     sitemap: 'https://thelostcabins.in/sitemap.xml',
   };
 }
